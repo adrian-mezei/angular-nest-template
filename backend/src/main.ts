c37d4a0e07d477df7b7b247787a21f97284ce8ce
Adrián Mezei <mezei.adrian@gmail.com>
2020-12-22 20:34:34 +0100

    Source map support added for logging stack trace of .ts files.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentVariables } from './env.validation';
+import 'source-map-support/register';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
