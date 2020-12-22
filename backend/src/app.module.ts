48c31628362a96369f67a2c47ae3a277604dc631
Adrián Mezei <mezei.adrian@gmail.com>
2020-12-22 15:52:31 +0100

    Users module fixed.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [UsersModule, AuthModule],
+    imports: [AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
