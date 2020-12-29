f874acc123fd8cde1968689289c584b0c9c70c30
Adrián Mezei <mezei.adrian@gmail.com>
2020-12-30 00:54:20 +0100

    Jest reconfigured to exclude modules and config moved to separate file.

---
+++ backend/jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => {
+    return {
+        testEnvironment: 'node',
+        moduleFileExtensions: ['js', 'json', 'ts'],
+        rootDir: 'src',
+        testRegex: '.*\\.spec\\.ts$',
+        transform: {
+            '^.+\\.(t|j)s$': 'ts-jest',
+        },
+        collectCoverageFrom: ['**/*.(t|j)s'],
+        coverageDirectory: '../coverage',
+        coveragePathIgnorePatterns: ['/node_modules/', '.*\\.module\\.ts$'],
+    };
+};
