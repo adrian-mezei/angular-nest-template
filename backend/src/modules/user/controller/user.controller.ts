60e5b3cb5b6bcd34dbca8da426fdd05163baa311
Adrián Mezei <mezei.adrian@gmail.com>
2021-01-01 18:46:00 +0100

    Added tag for swagger.

--- backend/src/modules/user/controller/user.controller.ts
+++ backend/src/modules/user/controller/user.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserEntity } from '../entities/user.entity';
 import { UserService } from '../service/user.service';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
