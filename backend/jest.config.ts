3d2ac052ae0a692de61c1e9264cbadd7d219bc21
Adrián Mezei <mezei.adrian@gmail.com>
2021-05-23 18:56:21 +0200

    test: require 100 percent test coverage

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -19,5 +19,13 @@ export default async (): Promise<Config.InitialOptions> => {
             'jwt-auth\\.guard\\.ts',
             'migrations',
         ],
+        coverageThreshold: {
+            global: {
+                branches: 100,
+                functions: 100,
+                lines: 100,
+                statements: 100,
+            },
+        },
     };
 };
